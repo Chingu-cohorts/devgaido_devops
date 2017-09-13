4bbcdac293181fd4908abf5d9f69d27333bd5a6e
Jim Medlock <jdmedlock@gmail.com>
2017-09-13 16:01:48 -0500

    Added common logging function

--- runtime/dglbackup.sh
+++ runtime/dglbackup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#------------------------------------------------------------------------------
+#==============================================================================
 # Script:  dglbackup.sh
 # Purpose: Create a "hot" backup of the devGaido Mongo database on the local
-#          application server.
+#          application server. 
 #
 # TODO: only backup catalog and devGaido
 # TODO: use 'docker exec -it <CONTAINER_NAME> /bin/bash' to execute in a 
@@ -12,41 +12,80 @@
 #       container fire this script from within the Mongo container. Look at 
 #       pid to share processes between containers --> 
 #          https://www.guidodiepen.nl/2017/04/accessing-container-contents-from-another-container/
+#==============================================================================
+
+# Initialize the log file
+LOGFILE="./dgllog.txt"
+echo "" > $LOGFILE
+
+#------------------------------------------------------------------------------ 
+# Function: log
+# Purpose:  Write and informational message to the console and/or log file
+# Usage:    log <message> <target>
+#             where: 
+#                <message> - Informational text to be written
+#                <target>  - Defines where the text is to be written to
+#                            'log'     - write only to the log file
+#                            'console' - write only to the console
+#                            'both'    - write to both the log file AND console
+#             
 #------------------------------------------------------------------------------
+log () {
+  MESSAGE=$1
+  TARGET=${2:-console}
+  if [ "${MESSAGE:-0}" == 0 ]; then
+    echo "Aborting! Log message omitted. MESSAGE="$MESSAGE
+    exit 1
+  fi
 
-echo "Starting devGaido 'hot' backup..."
+  case "$TARGET" in
+    "console")
+      echo "$MESSAGE"
+      ;;
+    "log")
+      echo "$MESSAGE">>$LOGFILE
+      ;;
+    "both")
+      echo "$MESSAGE"
+      echo "$MESSAGE">>$LOGFILE
+      ;;
+    *)
+      echo "Aborting! Invalid target specified. TARGET="$TARGET
+  esac
+}
 
-echo "...Verifying that MongoDB instance is available..."
+#------------------------------------------------------------------------------
+# Mainline Script Logic 
+#------------------------------------------------------------------------------ 
+
+log "...Verifying that MongoDB instance is available..."
 # Try to display Mongo statistics to check if the instance is running.
 # '$?'. will be 0 if the command succeeds.
-echo "Verifying that MongoDB is running" > ./dgllog.txt
-mongo --eval "db.stats()" >> ./dgllog.txt
+log "Verifying that MongoDB is running" both
+mongo --eval "db.stats()" >> $LOGFILE
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-    echo "......MongoDB is not running. Please start before attempting backup"
-    echo "Aborting backup. MongoDB not running" >> ./dgllog.txt
+    log "...Aborting backup. MongoDB not running" both
     exit 1
 fi
 
-echo "...Backing up devGaido database..."
+log "...Backing up devGaido database..."
 # Create the local backup directory if it does not exist
 # [ ! -d ./dglbackups ] && mkdir -p ./dglbackups
 if test ! -d ./dglbackups
 then
-    echo "......Creating local backup directory ./dglbackups"
+    log "......Creating local backup directory ./dglbackups" both
     mkdir -p ./dglbackups
-    echo "Create missing backup directory" >> ./dgllog.txt
+    log "Create missing backup directory" log
 fi
 
 mongodump -o ./dglbackups >> ./dgllog.txt
 RESULT=$?
 if [ $RESULT = 0 ]; then
-  echo "Backup successfully completed. Return code=" $RESULT
-  echo "Backup successfully completed. Return code=" $RESULT >> ./dgllog.txt
+  log "Backup successfully completed. Return code=$RESULT" both
 else
-  echo "Backup was unsuccessful. Return code=" $RESULT
-  echo "Backup was unsuccessful. Return code=" $RESULT >> ./dgllog.txt
+  log "Backup was unsuccessful. Return code=$RESULT" both
 fi
 
-echo "Ending devGaido 'hot' backup"
+log "Ending devGaido 'hot' backup" both
 exit 0
