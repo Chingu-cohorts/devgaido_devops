a6d8c6fd975e775602b2529f2f9a3168c063918d
jdmedlock <jdmedlock@gmail.com>
2017-09-14 19:07:05 -0500

    Add timestamp to log function
    Change to backup only admin and devGaido databases

--- runtime/dglbackup.sh
+++ runtime/dglbackup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Common Bash functions
-source dglog.sh
+source ./dglog.sh
 
 #==============================================================================
 # Script:  dglbackup.sh
 # Purpose: Create a "hot" backup of the devGaido Mongo database on the local
 #          application server. 
 #
-# TODO: only backup catalog and devGaido
 # TODO: use 'docker exec -it <CONTAINER_NAME> /bin/bash' to execute in a 
 #       container. 
 # TODO: Look at container-to-container communication. E.g. have automation
@@ -17,39 +16,49 @@ source dglog.sh
 #          https://www.guidodiepen.nl/2017/04/accessing-container-contents-from-another-container/
 #==============================================================================
 
+LOGFILE="./dgllog.txt"
+
 #------------------------------------------------------------------------------
 # Mainline Script Logic 
 #------------------------------------------------------------------------------ 
 
-initlog "./dgllog.txt"
-log "...Verifying that MongoDB instance is available..."
+initlog "$LOGFILE" "Starting devGaido Hot Backup..."
+
 # Try to display Mongo statistics to check if the instance is running.
 # '$?'. will be 0 if the command succeeds.
-log "Verifying that MongoDB is running" both
+log $LOGFILE "...Verifying that MongoDB instance is running..." both
 mongo --eval "db.stats()" >> $LOGFILE
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-    log "...Aborting backup. MongoDB not running" both
+    log $LOGFILE "...Aborting backup. MongoDB not running" both
     exit 1
 fi
 
-log "...Backing up devGaido database..."
+log $LOGFILE "...Backing up admin & devGaido databases..."
 # Create the local backup directory if it does not exist
 # [ ! -d ./dglbackups ] && mkdir -p ./dglbackups
 if test ! -d ./dglbackups
 then
-    log "......Creating local backup directory ./dglbackups" both
+    log $LOGFILE "......Creating local backup directory ./dglbackups" both
     mkdir -p ./dglbackups
-    log "Create missing backup directory" log
+    log $LOGFILE "...Create missing backup directory" log
 fi
 
-mongodump -o ./dglbackups >> ./dgllog.txt
+mongodump --verbose --db admin -o ./dglbackups >> $LOGFILE
 RESULT=$?
 if [ $RESULT = 0 ]; then
-  log "Backup successfully completed. Return code=$RESULT" both
+  log $LOGFILE "...Backup of admin db successfully completed. Return code=$RESULT" both
 else
-  log "Backup was unsuccessful. Return code=$RESULT" both
+  log $LOGFILE "...Backup of admin db was unsuccessful. Return code=$RESULT" both
 fi
 
-log "Ending devGaido 'hot' backup" both
+mongodump --verbose --db devgaido -o ./dglbackups >> $LOGFILE
+RESULT=$?
+if [ $RESULT = 0 ]; then
+  log $LOGFILE "...Backup of devgaido db successfully completed. Return code=$RESULT" both
+else
+  log $LOGFILE "...Backup of devgaido db was unsuccessful. Return code=$RESULT" both
+fi
+
+log $LOGFILE "Ending devGaido 'hot' backup" both
 exit 0
--- runtime/dglog.sh
+++ runtime/dglog.sh
@@ -1,51 +1,76 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------ 
+# Function: currentTS
+# Purpose:  Generate a timestamp for the current time
+# Usage:    currentTS
+#------------------------------------------------------------------------------ 
+currentTS () {
+  TS=`date "+%Y-%m-%dT%H:%M:%S"`
+  echo $TS
+}
+
+#------------------------------------------------------------------------------ 
 # Function: initlog
 # Purpose:  Initialize the disk log file the log function will be adding 
 #           entries to
-# Usage:    initlog <log-file-name>
+# Usage:    initlog <log-file-name> <init-message>
+#           where:
+#             <log-file-name> - Path and file name of the log file
+#             <init-message>  - Initialization message to be added to first
+#                               line of the log file
 #------------------------------------------------------------------------------ 
 initlog () {
   LOGFILE=$1
+  INITMSG=$2
   if [ "${LOGFILE:-0}" == 0 ]; then
     echo "Aborting: Log file name omitted. LOGFILE="$LOGFILE
   fi
-  echo "" > $LOGFILE
+  
+  TS=$(currentTS)
+  echo "$TS: $INITMSG" > $LOGFILE
 }
 
 #------------------------------------------------------------------------------ 
 # Function: log
 # Purpose:  Write and informational message to the console and/or log file
-# Usage:    log <message> <target>
-#             where: 
-#                <message> - Informational text to be written
-#                <target>  - Defines where the text is to be written to
-#                            'log'     - write only to the log file
-#                            'console' - write only to the console
-#                            'both'    - write to both the log file AND console
-#             
+# Usage:    log <log-file-name> <message> <target>
+#           where: 
+#             <log-file-name> - path and file name of the log file. The log
+#                               file must have been previously created with 
+#                               initlog.
+#             <message>       - Informational text to be written
+#             <target>        - Defines where the text is to be written to
+#                               'log'     - write only to the log file
+#                               'console' - write only to the console
+#                               'both'    - write to both the log file AND 
+#                                           the console
 #------------------------------------------------------------------------------
 log () {
-  MESSAGE=$1
-  TARGET=${2:-console}
+  LOGFILE=$1
+  MESSAGE=$2
+  TARGET=${3:-console}
+  if [ "${LOGFILE:-0}" == 0 ]; then
+    echo "$$TS: Aborting! Log file name omitted. LOGFILE="$LOGFILE
+  fi
   if [ "${MESSAGE:-0}" == 0 ]; then
-    echo "Aborting! Log message omitted. MESSAGE="$MESSAGE
+    echo "$TS: Aborting! Log message omitted. MESSAGE="$MESSAGE
     exit 1
   fi
 
+  TS=$(currentTS)
   case "$TARGET" in
     "console")
-      echo "$MESSAGE"
+      echo "$TS: $MESSAGE"
       ;;
     "log")
-      echo "$MESSAGE">>$LOGFILE
+      echo "$TS: $MESSAGE">>$LOGFILE
       ;;
     "both")
-      echo "$MESSAGE"
-      echo "$MESSAGE">>$LOGFILE
+      echo "$TS: $MESSAGE"
+      echo "$TS: $MESSAGE">>$LOGFILE
       ;;
     *)
-      echo "Aborting! Invalid target specified. TARGET="$TARGET
+      echo "$TS: Aborting! Invalid target specified. TARGET="$TARGET
   esac
 }
